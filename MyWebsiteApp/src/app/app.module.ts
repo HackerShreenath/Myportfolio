3a92fe256be00ba25c58d085f578c22640450ff7
Shreenath sp <shreenathsp00@gmail.com>
2025-02-25 13:11:01 +0530

    corrected one

---
+++ MyWebsiteApp/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';  // Import HomeComponent
+
+@NgModule({
+  declarations: [
+    AppComponent,  
+    HomeComponent  
+  ],
+  imports: [BrowserModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
